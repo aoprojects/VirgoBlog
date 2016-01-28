907e5a88ca0e98625059e3fdf6f1cc4c680f6a41
Alison Ostendorf <aostendorf@gmail.com>
2016-01-28 17:15:38 -0500

    try to create drop down of #articles_summary with jquery. Not working yet.

--- jquery.js
+++ jquery.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-	$(this).load(function() {
-		$(this).slideToggle(".tagline");
+
+	$("#articles_summary").click(function() {
+		$(this).next().fadeToggle("fast");
+		$(this).next().toggleClass("collapse");
 	});
 
 
-});
 
 // I realize that this code is not doing the effect yet. I will learn more later.
