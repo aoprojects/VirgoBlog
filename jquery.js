c6fce07a5e96eafb25d5feb1094f55e005576306
Alison Ostendorf <aostendorf@gmail.com>
2016-01-28 11:32:36 -0500

    edit tagline of blog page and try to use jquery for slide or fade in tagline

--- jquery.js
+++ jquery.js
@@ -1,1 +1,9 @@
-jquery.js
+$(document).ready(function() {
+	$(this).load(function() {
+		$(this).slideToggle(".tagline");
+	});
+
+
+});
+
+// I realize that this code is not doing the effect yet. I will learn more later.
